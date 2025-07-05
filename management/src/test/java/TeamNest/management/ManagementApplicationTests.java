6d82edd8131b6148409aed05f277e0548abb15d4
JoseManuelGraoSoto <jmgs24@alu.ua.es>
2025-07-05 12:57:33 +0200

    Initial commit with the main project

---
+++ management/src/test/java/TeamNest/management/ManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package TeamNest.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
